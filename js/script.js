a6984255f7983d076b4536aaf79fa2b45a98695b
Tim Sarvey <timsarvey@gmail.com>
2015-09-12 22:44:38 -0400

    Fix checkbox for added items

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 $( document ).ready(function() {
-    $("input").on("click", function(){
+    $("input:checkbox").on("click", clicked);
+    
+    $("#addItem").click(function(){
+    	var item = $("#newitem").val();
+    	if(item != ''){
+    		var newItemHTML = "<li><input type='checkbox'>" + item + "</li>";
+    		$(".current-list").prepend(newItemHTML);
+    		$(".current-list > li:first-child > input:checkbox").on("click", clicked);
+    	}
+    });
+});
+
+function clicked(){
     	if($(this).is(":checked")){
     		$(this).parent().addClass("checked").detach();
     		$(".removed-list").prepend($(this).parent());
+    	
     	}else{
     		$(this).parent().removeClass("checked").detach();
     		$(".current-list").prepend($(this).parent());
     	}	
-    });
-});
+    	console.log("clicked");
+    }
